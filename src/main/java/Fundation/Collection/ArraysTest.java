245bee39ce0f41a781e5db1848170540105c7cdd
dell <flupingo7@yeah.net>
2021-09-17 17:00:29 +0800

    Commit5

--- src/main/java/Fundation/Collection/ArraysTest.java
+++ src/main/java/Fundation/Collection/ArraysTest.java
@@ -15,6 +15,7 @@ public class ArraysTest {
         testCollections();
         System.out.println("1");
         System.out.println("2");
+        System.out.println("3");
     }
 
     static void testArrays() {
